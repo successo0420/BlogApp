6a87537f2d7b85353864c92005975f8db262a177
Success0406 <success@nynthlabs.com>
2023-09-05 22:56:22 -0400

    Fixing CSS and adding TODO's

--- main.py
+++ main.py
@@ -10,19 +10,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm, SearchForm
 
-# Add User Profile Function With Contact Me
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
