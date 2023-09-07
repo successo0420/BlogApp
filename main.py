e794258731554e24faed0647f439fc5285aef9a1
successo0420 <126357345+successo0420@users.noreply.github.com>
2023-09-07 11:23:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -65,11 +65,11 @@ class BlogPost(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(250))
     name = db.Column(db.String(100))
-    image_url = db.Column(db.String)
-    username = db.Column(db.String, unique=True)
+    image_url = db.Column(db.String(300))
+    username = db.Column(db.String(100), unique=True)
     # This will act like a list of BlogPost objects attached to each User.
     # The "author" refers to the author property in the BlogPost class.
     posts = relationship("BlogPost", back_populates="author")
