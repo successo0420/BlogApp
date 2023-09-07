df7623aa43f51bd97418394557e823c6038ee0de
successo0420 <126357345+successo0420@users.noreply.github.com>
2023-09-07 10:50:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ class BlogPost(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String())
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
     name = db.Column(db.String(100))
     image_url = db.Column(db.String)
     username = db.Column(db.String, unique=True)
